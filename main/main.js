ea9a4cce607253a627cb3adfd43f78c74b9e0791
tbremm <timothy.bremm@gmail.com>
2020-04-10 00:28:54 -0700

    cleanup

--- main/main.js
+++ main/main.js
@@ -2,9 +2,7 @@ const Constants = require("../config/constants");
 const Slack = require("./post_to_slack");
 const Foaas = require("./foaas");
 
-// Might be a better way to do this but
-// the API calls are all async and this is
-// what I found works (and online)
+
 async function main() {
     const foaas = new Foaas.Foaas();
     const slackbot = new Slack.Slackbot();
