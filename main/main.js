b8dee4e1f9391030b5d3e217ba3def39d4b21ef3
tbremm <timothy.bremm@gmail.com>
2020-04-10 00:45:45 -0700

    added try/catch

--- main/main.js
+++ main/main.js
@@ -4,16 +4,21 @@ const Foaas = require("./foaas");
 
 
 async function main() {
-    const foaas = new Foaas.Foaas();
-    const slackbot = new Slack.Slackbot();
-    const channel = Constants.DEFAULT_CHANNEL_ID;
-    const fo = await foaas.getFO(Constants.URL_FOAAS, "tucker", "Tim");
-    let text = fo.message + "\n" + fo.subtitle;
-    let slackProps = {
-        text: text,
-        channel: channel
-    };
-    slackbot.send(slackProps);
+    try {
+        const foaas = new Foaas.Foaas();
+        const slackbot = new Slack.Slackbot();
+        const channel = Constants.DEFAULT_CHANNEL_ID;
+        const fo = await foaas.getFO(Constants.URL_FOAAS, "tucker", "Tim");
+        let text = fo.message + "\n" + fo.subtitle;
+        let slackProps = {
+            text: text,
+            channel: channel
+        };
+        slackbot.send(slackProps);
+    } catch (error) {
+        console.log('main: ' + error);
+        process.exit(-1);
+    }
 }
 
 main();
