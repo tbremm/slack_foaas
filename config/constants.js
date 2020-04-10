d98d8af22256915d9e38724154802bba65fe452b
tbremm <timothy.bremm@gmail.com>
2020-04-09 22:06:11 -0700

    constants are back on the menu

---
+++ config/constants.js
@@ -0,0 +1,11 @@
+// Configure some basic defaults
+
+const URL_SLACK_POST_MESSAGE = "https://slack.com/api/chat.postMessage";
+const DEFAULT_SLACK_CHANNEL = "tb-dev";
+const DEFAULT_CHANNEL_ID = "GLFAXF5B5";
+
+module.exports = {
+    URL_SLACK_POST_MESSAGE: URL_SLACK_POST_MESSAGE,
+    DEFAULT_SLACK_CHANNEL,
+    DEFAULT_CHANNEL_ID: DEFAULT_CHANNEL_ID,
+}
